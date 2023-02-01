6272e9a71bc3118812f0c4b274c7e421038a1309
Abdibaset Bare <abdibaset.a.bare.25@dartmouth.edu>
2023-02-01 00:04:08 -0500

    step1: crawler complete

---
+++ crawler/crawler.c
@@ -0,0 +1,18 @@
+/* crawler.c --- crawler searching urls on a site
+ * 
+ * 
+ * Authors: Abdibaset Bare, Ian Kamweru and Nathaniel Mensah
+ * Created: Tue Jan 31 23:57:45 2023 (-0500)
+ * Version: 1.0
+ * 
+ * Description: programs that crawls websites and collects embedded site in the website 
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    printf("hello!\n");
+
+    exit(EXIT_SUCCESS);
+}
