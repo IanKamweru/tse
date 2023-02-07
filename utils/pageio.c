d4a5d57018272f008d6bc1b039e8d162ad41a04c
Ian <ian.kamweru.ug@dartmouth.edu>
2023-02-07 18:50:38 -0500

    pageload util

--- utils/pageio.c
+++ utils/pageio.c
@@ -4,7 +4,10 @@
  * Author: Ian Kamweru, Abdibaset, Nathaniel Mensah
  * Version: 1.0
  * 
- * Description:
+ * Description: pagesave saves an existing webpage to a file with a
+ * numbered name (e.g. 1,2,3 etc); pageload creates a new page by
+ * loading a numbered file. For pagesave, the directory must exist and
+ * be writable; for loadpage it must be readable.
  */
 
 #include "pageio.h"
@@ -50,11 +53,62 @@ int32_t pagesave(webpage_t *pagep, int id, char *dirnm){
 }
 
 /* 
- * pageload -- loads the numbered filename <id> in direcory <dirnm>
+ * pageload -- loads the numbered filename <id> in directory <dirnm>
  * into a new webpage
  *
  * returns: non-NULL for success; NULL otherwise
  */
 webpage_t *pageload(int id, char *dirnm){
-    return NULL;
+    if(!dirnm)
+        return NULL;
+
+    char path[1024];
+    sprintf(path, "%s/%d", dirnm, id);
+
+    FILE *file = fopen(path,"r");
+    if(file==NULL || access(path,R_OK) != 0) {
+        return NULL;
+    }
+
+    /* scan url */
+    char url[1024];
+    if(fscanf(file,"%s\n",url) != 1){
+        printf("Failed to read URL from file id: %d\n", id);
+        fclose(file);
+        return NULL;
+    }
+
+    /* scan depth and html_length */
+    int depth, html_length;
+    if (fscanf(file, "%d\n", &depth) != 1) {
+        printf("Failed to read depth from file id: %d\n", id);
+        fclose(file);
+        return NULL;
+    }
+
+    if (fscanf(file, "%d\n", &html_length) != 1) {
+        printf("Failed to read character count from file id: %d\n", id);
+        fclose(file);
+        return NULL;
+    }
+
+    /* scan the html */
+    char *html = malloc((html_length + 1) * sizeof(char));
+    int idx = 0;
+    char c;
+    while ((c = fgetc(file)) != EOF && idx < html_length) {
+        html[idx++] = c;
+    }
+
+    html[idx] = '\0';
+    fclose(file);
+
+    /* create webpage */
+    webpage_t *page = webpage_new(url,depth,html);
+    if(!page){
+        printf("Failed to initialize webpage\n");
+        return NULL;
+    }
+
+    return page;
 }
