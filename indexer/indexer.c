cc134d09cb7f544f76992ea94b6c99cfcc89a672
Ian <ian.kamweru.ug@dartmouth.edu>
2023-02-11 15:04:08 -0500

    queue of docs - step 4

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -16,24 +16,43 @@
 #include <ctype.h>
 #include "pageio.h"
 #include "hash.h"
+#include "queue.h"
 
 #define hsize 1000    // hashtable size
 
 static int total_count = 0;
 
-/* index entry struct */
+/* index entry struct 
+ *
+ * @param word - the word to add to the index
+ * @param documents - the queue of crawled docs containing the word
+*/
 typedef struct entry{
 	char *word;
-	int count;
-}entry_p;
+	queue_t *documents;
+}entry_t;
+
+/* document struct
+ *
+ * @param id - document id designated by crawler
+ * @param word_count - the count of a specific word in the index in this doc
+ */
+typedef struct document{
+	int id;
+	int word_count;
+} document_t;
 
 /* allocate entry */
-entry_p *new_entry(char *word, int count){
-	if (count < 0 || word == NULL)
+entry_t *new_entry(char *word){
+	if (!word)
 		return NULL;
 
-	entry_p *entry = malloc(sizeof(entry_p));
+	entry_t *entry = malloc(sizeof(entry_t));
 	if (!entry)
+		return NULL;
+
+	entry->documents = qopen();
+	if (entry->documents == NULL)
 		return NULL;
 
 	entry->word = malloc(strlen(word)+1);
@@ -41,19 +60,43 @@ entry_p *new_entry(char *word, int count){
 		return NULL;
 
 	strcpy(entry->word, word);
-	entry->count = count;
-	
+
 	return entry;
 }
 
-static bool searchfn(void *elementp, const void *searchkeyp){
-    entry_p *ep = (entry_p*)elementp;
+/* allocate document */
+document_t *new_doc(int id, int word_count){
+	document_t *dp = (document_t*)malloc(sizeof(document_t));
+	if(!dp)
+		return NULL;
+	
+	dp->id = id;
+	dp->word_count = word_count;
+	return dp;
+}
+
+/* searches for entry in the hash table */
+static bool entry_searchfn(void *elementp, const void *searchkeyp){
+    entry_t *ep = (entry_t*)elementp;
     return strcmp(ep->word,(char*)searchkeyp) == 0;
 }
 
-static void sum_fn(void* ep){
-	entry_p *p = (entry_p*)ep;
-	total_count += p->count;
+/* searches for document in queue */
+static bool doc_searchfn(void *elementp, const void *id){
+	document_t *dp = (document_t*)elementp;
+	return dp->id == *(int*)id;
+}
+
+/* total word count in the queue ie. word count for a specific word */
+static void queue_sum_fn(void* elementp){
+	document_t *dp = (document_t*)elementp;
+	total_count+=dp->word_count;
+}
+
+/* total word count */
+static void total_sum_fn(void* ep){
+	entry_t *p = (entry_t*)ep;
+	qapply(p->documents,queue_sum_fn);
 }
 
 static void NormalizeWord(char *word){
@@ -86,25 +129,34 @@ int main(void){
 
 	int pos = 0;
 	char *word;
-	entry_p *ep;
+	entry_t *ep;
 	hashtable_t *index = hopen(hsize);
 
 	while((pos=webpage_getNextWord(page,pos,&word)) > 0){
 		NormalizeWord(word);
 		if(word[0]!='\0'){
-			if (hsearch(index, searchfn, word, strlen(word))){
-				ep = (entry_p*)hsearch(index, searchfn, word, strlen(word));
-				ep->count = ep->count + 1;
+			if (hsearch(index, entry_searchfn, word, strlen(word))){
+				ep = (entry_t*)hsearch(index, entry_searchfn, word, strlen(word));
+				document_t *dp;
+				if((dp = qsearch(ep->documents,doc_searchfn,&id))){
+					dp->word_count = dp->word_count + 1;
+				}
+				else{
+					dp = new_doc(id,1);
+					qput(ep->documents,dp);
+				}
 			}
 			else{
-				ep = new_entry(word, 1);
+				ep = new_entry(word);
+				document_t *dp = new_doc(id,1);
+				qput(ep->documents,dp);
 				hput(index, ep, word, strlen(word));
 			}
 			printf("%s\n",word);
 		}
 	}
 
-	happly(index, sum_fn);
+	happly(index, total_sum_fn);
 	printf("Total word count in hashtable: %d\n", total_count);
 	return 0;
 }
