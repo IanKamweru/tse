de515e036ef5b7dcba251f568c34ed341fec868d
Ian <ian.kamweru.ug@dartmouth.edu>
2023-02-20 18:34:09 -0500

    step 4 initial commit

--- querier/query.c
+++ querier/query.c
@@ -24,11 +24,11 @@
 #define MAX_QUERY_LEN 512
 
 /**
- * @brief represents a ranked doc with id, rank and url
+ * @brief represents a ranked doc with id, ranked word_count and url
 */
 typedef struct doc {
     int id;
-    int rank;
+    int word_count;
     char *url;
 } rankedDoc_t;
 
@@ -95,14 +95,13 @@ static bool doc_searchfn(void *elementp, const void *id);
 /**
  * sets the ranked page url
  * 
- * @param id the crawled page id
+ * @param ranked_docs the queue of ranked docs
  * @param pagedir the directory containing crawled pages
- * @param url_buffer the pointer to the buffer to write the url into
 */
-static void set_url(int id, char *pagedir, char **url_buffer);
+static void get_url(queue_t *ranked_docs, char *pagedir);
 
 /**
- * finds the intersection between the ranked queue and token's queue out of place
+ * finds the intersection between two ranked queues
  * 
  * @param qp1 the ranked queue
  * @param qp2 the queue of docs containing a token
@@ -111,15 +110,14 @@ static void set_url(int id, char *pagedir, char **url_buffer);
 static queue_t* get_intersection(queue_t *qp1, queue_t *qp2);
 
 /**
- * finds the union between the ranked queue and token's queue in place
+ * finds the union between two ranked queues in place
  * ie. the result is concatenated into qp1, qp2 is restored to original state
  * 
  * @param qp1 the ranked queue
  * @param qp2 the queue of docs containing a token
- * @param pagedir directory of crawled pages. Used to get metadata ie url etc.
  * @return a pointer to a queue containing the union
 */
-static queue_t* get_union(queue_t *qp1, queue_t *qp2, char *pagedir);
+static queue_t* get_union(queue_t *qp1, queue_t *qp2);
 
 /*************************** MAIN ******************************/
 int main(void){
@@ -129,16 +127,17 @@ int main(void){
     hashtable_t *index = indexload(index_file);
 
     char query[MAX_QUERY_LEN];
-    char **tokenized_query, *token;
-    int num_tokens;
+    char **tokenized_query, *token, *curr_operator;
+    int num_tokens, top;
     entry_t *ep;
     rankedDoc_t *doc;
-    queue_t *ranked_docs;
-
+    queue_t *ranked_docs, **stack=NULL, *tmp, *qp1, *qp2;
 
     while(1){
         ranked_docs = qopen();
 		num_tokens = 0;
+        top = -1;
+        curr_operator = "";
 
         /* get input from stdin */
         if(get_input(query) != 0){
@@ -160,31 +159,46 @@ int main(void){
             continue;
         }
 
-        /* process each token */
-        for (int i = 0; i < num_tokens; i++) {
+        /* process tokens in Backus-Naur Form */
+        for(int i = 0; i < num_tokens; i++){
             token = tokenized_query[i];
-            if(strcmp(token, and)==0 || strcmp(token, or)==0)
+
+            /* if token is an operator, update current operator and continue */
+            if(strcmp(token, and)==0 || strcmp(token, or)==0){
+                curr_operator = token;
                 continue;
+            }
 
             ep = hsearch(index, token_searchfn, token, strlen(token));
-            if(ep){
-                if(i==0){
-                    /* first iteration - add all docs */
-                    ranked_docs = get_union(ranked_docs, ep->documents, pagedir);
-                } else {
-                    /* find intersection between current ranked and token's queue */
-                    ranked_docs = get_intersection(ranked_docs, ep->documents);
-                }
+            tmp = qopen();
+            if(ep){ // if token is present in index push its queue
+                tmp = get_union(tmp, ep->documents);
             }
-            else{ // token not in index
-                qclose(ranked_docs);
-                ranked_docs = NULL;
+            stack = (queue_t**) realloc(stack, sizeof(queue_t*) * (top + 2));
+            stack[++top] = tmp;
+            
+            /* if last operator is and, get intersect of prev two queues in stack */
+            if(strcmp(curr_operator, and)==0){
+                qp1 = stack[top--];
+                qp2 = stack[top--];
+                stack[++top] = get_intersection(qp1, qp2);
             }
         }
 
+        /* union everything left in the stack */
+        while(top>0){
+            qp1 = stack[top--];
+            qp2 = stack[top--];
+            stack[++top] = get_union(qp1, qp2);
+        }
+        ranked_docs = stack[top--];
+
+        /* set metadata -> url, title, heading */
+        get_url(ranked_docs, pagedir);
+
         /* print docs' rank & url */
         while((doc=qget(ranked_docs))){
-            printf("rank:%d doc:%d : %s\n",doc->rank,doc->id,doc->url);
+            printf("rank:%d doc:%d : %s\n",doc->word_count,doc->id,doc->url);
         }
 
         qclose(ranked_docs);
@@ -210,7 +224,7 @@ static int get_input(char *buffer){
 }
 
 static char** tokenize_query(char *query, int *num_tokens){
-    char **tokenized_query = NULL;
+    char **tokenized_query = NULL, *prev_token = NULL;
     char* token = strtok(query, " \t");
     int count = 0;
     while(token){
@@ -220,12 +234,24 @@ static char** tokenize_query(char *query, int *num_tokens){
             token = strtok(NULL, " \t");
             continue;
         }
+        if (prev_token && strcmp(prev_token, "and") != 0 && strcmp(prev_token, "or") != 0) {
+            // insert an implicit "and" before the current token
+            if(strcmp(token,"and") != 0 && strcmp(token,"or") != 0){
+                tokenized_query = realloc(tokenized_query, (count+1) * sizeof(char*));
+                tokenized_query[count] = malloc(strlen("and") + 1);
+                strcpy(tokenized_query[count], "and");
+                count++;
+            }
+        }
+
         tokenized_query = realloc(tokenized_query, (count+1) * sizeof(char*));
         tokenized_query[count] = malloc(strlen(token) + 1);
         strcpy(tokenized_query[count], token);
+        prev_token = token;
         token = strtok(NULL, " \t");
         count++;
     }
+
     *num_tokens = count;
     return tokenized_query;
 }
@@ -237,7 +263,7 @@ static rankedDoc_t* init_doc(int id, int rank){
         return NULL;
     }
     doc->id = id;
-    doc->rank = rank;
+    doc->word_count = rank;
     return doc;
 }
 
@@ -278,23 +304,24 @@ static queue_t* get_intersection(queue_t *qp1, queue_t *qp2){
     queue_t *intersect = qopen();
     if(!qp1 || !qp2 || !intersect)
         return NULL;
-    rankedDoc_t *doc; document_t *tmp;
+    document_t *doc;
+    rankedDoc_t *dp;
     int word_count, rank;
     while((doc = qget(qp1))){
-        if ((tmp=qsearch(qp2, doc_searchfn, &(doc->id)))) {
-            // Add doc to new queue
-            word_count = tmp->word_count;
-            rank = doc->rank;
-            doc->rank = word_count < rank ? word_count : rank;
+        if ((dp=qsearch(qp2, doc_searchfn, &(doc->id)))) {
+            word_count = dp->word_count;
+            rank = doc->word_count;
+            doc->word_count = word_count < rank ? word_count : rank;
             qput(intersect, doc);
-        } 
+        }
     }
+
     return intersect;
 }
 
-static queue_t* get_union(queue_t *qp1, queue_t *qp2, char *pagedir){
+static queue_t* get_union(queue_t *qp1, queue_t *qp2){
     queue_t *tmp = qopen();
-    if(!qp1 || !qp2 || !tmp)
+    if(!qp1 ||!qp2 || !tmp)
         return NULL;
     document_t *dp;
     rankedDoc_t *doc;
@@ -302,15 +329,15 @@ static queue_t* get_union(queue_t *qp1, queue_t *qp2, char *pagedir){
     while((dp = qget(qp2))){
         word_count = dp->word_count;
         if ((doc=qsearch(qp1, doc_searchfn, &(dp->id)))) {
-            rank = doc->rank;
-            doc->rank = rank + word_count;
+            rank = doc->word_count;
+            doc->word_count = rank + word_count;
         } else{
             doc = init_doc(dp->id,word_count);
-            set_url(dp->id, pagedir, &(doc->url));
             qput(qp1, doc);
         }
         qput(tmp, dp);
     }
+
     // restore qp2
     while((dp = qget(tmp)))
         qput(qp2, dp);
@@ -319,16 +346,25 @@ static queue_t* get_union(queue_t *qp1, queue_t *qp2, char *pagedir){
     return qp1;
 }
 
-static void set_url(int id, char *pagedir, char **url_buffer){
-    webpage_t *page;
-    char *url;
-    if((page = pageload(id, pagedir))){
-        url = webpage_getURL(page);
-        if((*url_buffer = malloc(strlen(url) + 1))){
-            strcpy(*url_buffer, url);
+static void get_url(queue_t *ranked_docs, char *pagedir){
+    rankedDoc_t *dp;
+    queue_t *tmp = qopen();
+    int id; webpage_t *page; char *url;
+    while((dp = qget(ranked_docs))){
+        id = dp->id;
+        if((page = pageload(id, pagedir))){
+            url = webpage_getURL(page);
+            if((dp->url = malloc(strlen(url) + 1))){
+                strcpy(dp->url, url);
+            }
         }
+        qput(tmp, dp);
         webpage_delete(page);
     }
+ 
+    while((dp = qget(tmp)))
+        qput(ranked_docs, dp);
+    qclose(tmp);
 }
 
 static bool token_searchfn(void *elementp, const void *key){
