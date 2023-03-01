791983ab16cfa9d0af81691eff6e4e713fc80226
Ian <ian.kamweru.ug@dartmouth.edu>
2023-02-28 23:08:00 -0500

    rename mutex to avoid naming conflict in crawler

--- utils/lhash.c
+++ utils/lhash.c
@@ -14,39 +14,39 @@
 #include <lhash.h>
 #include <pthread.h>
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_h = PTHREAD_MUTEX_INITIALIZER;
 
 /* lhopen -- opens a hash table with initial size hsize */
 hashtable_t* lhopen(uint32_t lhsize){
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex_h);
     lhash_t* lhtp = (lhash_t*) hopen(lhsize);
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex_h);
     return lhtp;
 }
 
 /* lhclose -- closes a hash table */
 void lhclose(lhash_t* lhtp){
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex_h);
     hclose((hashtable_t*)lhtp);
-    pthread_mutex_unlock(&mutex);
-    pthread_mutex_destroy(&mutex); // Destroy the mutex
+    pthread_mutex_unlock(&mutex_h);
+    pthread_mutex_destroy(&mutex_h); // Destroy the mutex
 }
 
 /* lhput -- puts an entry into a hash table under designated key 
  * returns 0 for success; non-zero otherwise
  */
 int32_t lhput(lhash_t* lhtp, void *ep, const char *key, int keylen){
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex_h);
     int32_t status = hput((hashtable_t*)lhtp,ep,key,keylen);
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex_h);
     return status;
 }
 
 /* lhapply -- applies a function to every entry in hash table */
 void lhapply(lhash_t* lhtp, void(*fn)(void *ep)){
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex_h);
     happly((hashtable_t*)lhtp, fn);
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex_h);
 }
 
 /* lhsearch -- searchs for an entry under a designated key using a
@@ -54,9 +54,9 @@ void lhapply(lhash_t* lhtp, void(*fn)(void *ep)){
  */
 void* lhsearch(lhash_t *lhtp, bool(*searchfn)(void *ep, const void *searchkeyp), 
                 const char *key, int keylen){
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex_h);
     void* entry = hsearch((hashtable_t*)lhtp, searchfn, key, keylen);
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex_h);
     return entry;
 }
 
@@ -66,8 +66,8 @@ void* lhsearch(lhash_t *lhtp, bool(*searchfn)(void *ep, const void *searchkeyp),
  */
 void* lhremove(lhash_t *lhtp, bool(*searchfn)(void *ep, const void *searchkeyp), 
                 const char *key, int keylen){
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex_h);
     void* data = hremove((hashtable_t*)lhtp, searchfn, key, keylen);
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex_h);
     return data;
 }
